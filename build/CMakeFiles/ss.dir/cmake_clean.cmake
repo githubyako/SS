3b252286e0fcf800d36cc53b4e683cf002152501
githubyako <roux.nse@gmail.com>
2015-11-30 15:41:34 +0100

    init commit

---
+++ build/CMakeFiles/ss.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ss.dir/main.o"
+  "ss.pdb"
+  "ss"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ solarsys.h
@@ -0,0 +1,6 @@
+#ifndef SOLARSYS_H
+#define SOLARSYS_H
+
+class SOLARSYS_H
+
+#endif
